6e23ebbf89fd19a2f8ea93f748a73d1f05848133
DESKTOP-LE79157\Riko <kizliakovich2@gmail.com>
2021-10-26 00:57:05 +0300

    Added some first unit tests.

--- Unit-Tests-Andrey/UnitTest1.cs
+++ Unit-Tests-Andrey/UnitTest1.cs
@@ -1,5 +1,9 @@
 using NUnit.Framework;
 using PizzaStore;
+using PizzaStore.Services;
+using PizzaStore.Validators;
+using PizzaStore.Models;
+using System;
 
 namespace Unit_Tests_Andrey
 {
@@ -10,23 +14,60 @@ namespace Unit_Tests_Andrey
         {
         }
 
-        [TestCase(10, 2, 12)]
-        [TestCase(0, 0, 0)]
-        [TestCase(0, 1, 1)]
-        [TestCase(1, 0, 1)]
-        [TestCase(-10, -2, -12)]
-        [TestCase(-1, 2, 1)]
-        //[TestCase (-1,-2,0)]
-        public void PlusArgumentsTest(double firstArgument, double secondArgument, double expectedResult)
-        {
-            double actualResult = Menu.MakeOrder(firstArgument, secondArgument);
-            Assert.AreEqual(expectedResult, actualResult);
+        [TestCase("Andrey", 2, "Andrey")]
+        [TestCase("valera01", 1, "valera01")]
+        [TestCase("Andrey", 0, "Andrey")]
+        [TestCase("007", 1, "007")]
+        //[TestCase("007", 1, "1007")]
+
+        public void CreateUserNameArgumentTest(string name, double amountExpected, string nameExpected)
+        {        
+            var userActual = new UserService(new UserValidator()).CreateUser(name, amountExpected);
+            Assert.AreEqual(nameExpected, userActual.Name);
         }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
+        [TestCase("Andrey", 2, 2)]
+        [TestCase("valera01", 1, 1)]
+        [TestCase("Andrey", 0, 0)]
+        [TestCase("007", 1, 1)]
+        //[TestCase("007", 1, 2)]
+
+        public void CreateUserAmountArgumentTest(string name, double amount, double amountExpected)
+        {        
+            var userActual = new UserService(new UserValidator()).CreateUser(name, amount);
+            Assert.AreEqual(amountExpected, userActual.Amount);
         }
+
+        [TestCase("Andrey ", 2, "Andrey  is invalid.")]
+        [TestCase("!#$", 2, "!#$ is invalid.")]
+        [TestCase("Boris", 2, "boris is invalid.")]
+        //[TestCase("007", 1, "1007")]
+
+        public void CreateUserCatchArgumentErrorsTest(string name, double amount, string errorExpected)
+        {
+            try
+            {
+                var userActual = new UserService(new UserValidator()).CreateUser(name, amount);
+            }
+            catch (Exception ex)
+            {
+                //Assert.Throws(ex, errorExpected);
+                Assert.AreEqual(errorExpected, ex.Message.ToString());               
+            }            
+        }
+
+        //[TestCase("Andrey", 2, "Andrey", 2)]
+        //[TestCase("valera01", 1, "valera01", 1)]
+        //[TestCase("Andrey", 0, "Andrey", 0)]
+        //[TestCase("007", 1, "007", 1)]
+        //[TestCase("007", 1, "007", 1)]
+
+        //public void CreateUserTest(string name, double amount, string nameExpected, double amountExpected)
+        //{
+        //    var expectedResult = new User(nameExpected, amountExpected);
+        //    var userActual = new UserService(new UserValidator()).CreateUser(name, amount);
+        //    var actualResult = new User(userActual.Name, userActual.Amount);
+        //    Assert.AreEqual(expectedResult, actualResult);
+        //}
     }
 }
