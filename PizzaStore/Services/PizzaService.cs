b4b91f1fea79d513315db19f66364b95203c52ae
DESKTOP-M25GUR8\Kostiantin <k.galchenko27@gmail.com>
2021-10-27 02:56:13 +0300

    Создана ветка рефакторинга. Убраны коментарии.

--- PizzaStore/Services/PizzaService.cs
+++ PizzaStore/Services/PizzaService.cs
@@ -30,7 +30,7 @@ namespace PizzaStore.Services
             return _pizza;
         }
 
-        public User PayForPizza(User user) // Нужно проверить что у юзера пица-прайс поменялся. Будет две переменные р
+        public User PayForPizza(User user) 
         {
             user.Amount -= _pizza.Price;
 
