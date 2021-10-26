afed1aeb2385222edf1d4bcbe61de45d0f113158
DESKTOP-LE79157\Riko <kizliakovich2@gmail.com>
2021-10-26 22:22:19 +0300

    Added half of unit tests amount.

--- Unit-Tests-Andrey/UnitTest1.cs
+++ Unit-Tests-Andrey/UnitTest1.cs
@@ -18,7 +18,6 @@ namespace Unit_Tests_Andrey
         [TestCase("valera01", 1, "valera01", 1)]
         [TestCase("Andrey", 0, "Andrey", 0)]
         [TestCase("007", 1, "007", 1)]
-        //[TestCase("007", 1, "1007", 1)]
 
         public void CreateUserTest(string name, double amount, string nameExpected, double amountExpected)
         {
@@ -28,24 +27,69 @@ namespace Unit_Tests_Andrey
         }
 
         [TestCase("!#$", 2, "!#$ is invalid.")]
-        [TestCase("Boris", 2, "Boris is invalid.")]
-        [TestCase("Boris!", 2, "   is invalid.")]
+        [TestCase("Boris!", 2, "Boris! is invalid.")]
         [TestCase("Boris", -1, "-1 is invalid.")]
 
         public void CreateUserCatchArgumentErrorsTest(string name, double amount, string errorExpected)
         {
-
-            Assert.Throws<ArgumentException>( delegate { var userActual = new UserService(new UserValidator()).CreateUser(name, amount); }, errorExpected);
-
-            //try
-            //{
-            //    var userActual = new UserService(new UserValidator()).CreateUser(name, amount);
-            //}
-            //catch (Exception ex)
-            //{
-            //    //Assert.Throws(ex, errorExpected);
-            //    Assert.AreEqual(errorExpected, ex.Message.ToString());               
-            //}            
+            Assert.Throws<ArgumentException>( delegate { var userActual = new UserService(new UserValidator()).CreateUser(name, amount); }, errorExpected);        
         }
+
+        [TestCase("Andrey", true)]
+        [TestCase("valera01", true)]
+        [TestCase("#$%", false)]
+        [TestCase("Bor!s", false)]
+
+        public void UserValidatorNameTest(string name, bool expectedResult)
+        {
+            var validatorActual = new UserValidator();
+            bool actualResult = validatorActual.IsNameValid(name);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("1", true)]
+        [TestCase("0", false)]
+        [TestCase("-1", false)]
+
+        public void UserValidatorAmountTest(double amount, bool expectedResult)
+        {
+            var validatorActual = new UserValidator();
+            bool actualResult = validatorActual.IsAmountValid(amount);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("3", true)]
+        [TestCase("2", true)]
+        [TestCase("1", true)]
+        [TestCase("0", false)]
+        [TestCase("-1", false)]
+        [TestCase("4", false)]
+
+        public void IsPizzaTypeValidTest(string pizzaNumber, bool expectedResult)
+        {
+            PizzaValidator pizzaValidator = new PizzaValidator();
+            bool actualResult = pizzaValidator.IsPizzaTypeValid(pizzaNumber, out PizzaType pizzaType);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("1", "California", 8)]
+        [TestCase("2", "Detroit", 10)]
+        [TestCase("3", "Neapolitan" , 12)]
+
+        public void ChoosePizzaTest(string pizzaNumber, string pizzaNameExpected, double pizzaPrice)
+        {
+            PizzaService pizzaService = new PizzaService(new PizzaValidator());
+            Assert.AreEqual(pizzaNameExpected, pizzaService.ChoosePizza(pizzaNumber).Name);
+            Assert.AreEqual(pizzaPrice, pizzaService.ChoosePizza(pizzaNumber).Price);
+        }
+
+
+        //{PizzaType.California} (8$) , (2){PizzaType.Detroit}(10$), (3){PizzaType.Neapolitan}(12$)
+
+        //PizzaService.ChoosePizza(string pizzaName)
+        //PizzaService.PayForPizza(User user)
+        //PizzaService.CreatePizza
+        //PizzaService.BakePizza
+        //PizzaIngredientsService.GetIngredientsByPizzaType(PizzaType pizzaType)
     }
 }
