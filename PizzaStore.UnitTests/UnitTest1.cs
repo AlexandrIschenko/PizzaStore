01d90e01dea43225b33ff1167c689bf54ae05523
surface\artem <artem.slobodianiuk@gmail.com>
2021-10-26 11:18:01 +0300

    Unit tests project was created

---
+++ PizzaStore.UnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using PizzaStore;
+
+namespace PizzaStore.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
