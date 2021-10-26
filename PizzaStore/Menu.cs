a5fcd0729393daca9e5c70f9273a2582d3fc4b72
Irina-Koida <91370760+Irina-Koida@users.noreply.github.com>
2021-10-27 01:57:08 +0300

    added a little check to Menu.MakeOrder

--- PizzaStore/Menu.cs
+++ PizzaStore/Menu.cs
@@ -17,8 +17,29 @@ namespace PizzaStore
 
         public void MakeOrder()
         {
-            Console.WriteLine($"Choose any pizza: (1){PizzaType.California} (8$) , (2){PizzaType.Detroit}(10$), (3){PizzaType.Neapolitan}(12$)");
-            var pizzaType = Console.ReadLine();
+            var pizzaType = " ";
+            while (pizzaType == " ")
+            {
+                Console.WriteLine($"Choose any pizza: (1){PizzaType.California} (8$) ," +
+                    $" (2){PizzaType.Detroit}(10$), " +
+                    $"(3){PizzaType.Neapolitan}(12$) and press \"Enter\"");
+                pizzaType = Console.ReadLine().Trim();
+                foreach (char x in pizzaType)
+                {
+                    if (!char.IsLetter(x))
+                    {
+                        pizzaType = " ";
+                        Console.WriteLine("Wrong choise. Try again.");
+                        break;
+                    }
+                }
+                if (pizzaType.Length == 0)
+                {
+                    pizzaType = " ";
+                    Console.WriteLine("The string is empty.");
+                    continue;
+                }
+            }
 
             var pizza = _pizzaService.ChoosePizza(pizzaType);
             _pizzaService.PayForPizza(_user);
